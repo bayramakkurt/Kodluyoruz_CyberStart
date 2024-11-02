09e3d8dbbb79af1a47bacc0833b097a08eda7253
bayramakkurt <115426400+bayramakkurt@users.noreply.github.com>
2024-11-02 19:59:44 +0300

    Update OklidCozum.py

--- Hafta-2_ÖklidProblem/OklidCozum.py
+++ Hafta-2_ÖklidProblem/OklidCozum.py
@@ -10,14 +10,14 @@ points=[(8, 5),
  (1, 5)]
 distances=[]
 
-def euclideanDistance(nokta1,nokta2):
-    x2,y2=nokta2
-    x1,y1=nokta1
+def euclideanDistance(point1,point2):
+    x2,y2=point2
+    x1,y1=point1
     return (((x2-x1)**2)+((y2-y1)**2))**(1/2)
     
 for i in range(len(points)-1):
-    mesafe=euclideanDistance(points[i],points[i+1])
-    distances.append(mesafe)
+    distance=euclideanDistance(points[i],points[i+1])
+    distances.append(distance)
 
 min=distances[0]
 
